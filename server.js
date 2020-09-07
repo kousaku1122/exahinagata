663955b420a22f906caf1a3f6e9c4348ccce59c3
kousaku1122 <kou1122saku@gmail.com>
2020-09-07 09:40:44 +0000

    ✊🙏 Checkpoint ./server.js:1285359/2442 ./views/index.html:1285359/806

--- server.js
+++ server.js
@@ -12,14 +12,37 @@ app.get('/', (request, response) => {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-app.post('/saveUser', function(req, res){
+app.get('/findUsers', function(req, res){
   MongoClient.connect(mongouri, function(error, client) {
     const db = client.db(process.env.DB); // 対象 DB
     const colUser = db.collection('users'); // 対象コレクション
-    const user = {name: '山田', age:30}; // 保存対象
-    colUser.insertOne(user, function(err, result) {
-      res.sendStatus(200); // HTTP ステータスコード返却
+
+    // 検索条件（名前が「エクサくん」ではない）
+    // 条件の作り方： https://docs.mongodb.com/manual/reference/operator/query/
+    const condition = {name:{$ne:'エクサくん'}};
+
+    colUser.find(condition).toArray(function(err, users) {
+      res.json(users); // レスポンスとしてユーザを JSON 形式で返却
       client.close(); // DB を閉じる
+    });
+  });
+});
+
+app.post('/saveUser', function(req, res){
+  let received = '';
+  req.setEncoding('utf8');
+  req.on('data', function(chunk) {
+    received += chunk;
+  });
+  req.on('end', function() {
+    MongoClient.connect(mongouri, function(error, client) {
+      const db = client.db(process.env.DB); // 対象 DB
+      const colUser = db.collection('users'); // 対象コレクション
+      const user = JSON.parse(received); // 保存対象
+      colUser.insertOne(user, function(err, result) {
+        res.sendStatus(200); // HTTP ステータスコード返却
+        client.close(); // DB を閉じる
+      });
     });
   });
 });
